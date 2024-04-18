95340b9ec97c28e0ccde53c8cdc9e8de16de501e
Zander2895 <jacquesalexander71@gmail.com>
2024-04-18 19:19:28 -0400

    New Commit

--- Arithmetic Operation in Daily Life.py
+++ Arithmetic Operation in Daily Life.py
@@ -1,10 +1,10 @@
 #Calculate the total cost of three items
-grocery_bill = 4.25+6.52+6.98 
+grocery_expense = 4.25+6.52+6.98 
 
-print(grocery_bill)
+print(grocery_expense)
 
 #Calculate bank interest
 bank_interest = 10000 * 0.07
-bank_interest_two = 10000 + bank_interest 
+bank_account_amount = 10000 + bank_interest 
 
-print(bank_interest_two)
+print(bank_account_amount)
